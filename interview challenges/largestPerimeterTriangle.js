2a3ba350daab92319ac40362bb9d1bbba7a3400d
Adam Blomberg <adamablomberg@gmail.com>
2022-10-11 22:16:24 -0500

    cleared console logs from triangle problem and practiced with linked lists

--- interview challenges/largestPerimeterTriangle.js
+++ interview challenges/largestPerimeterTriangle.js
@@ -1,7 +1,6 @@
 //Given an integer array nums, return the largest perimeter of a triangle with a non-zero area,formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
 var largestPerimeter = function(nums) {
   nums.sort((a,b) => b-a)
-  console.log(nums);
   for(var i = 0; i < nums.length -2; i++){
     if(nums[i+2]+nums[i+1]>nums[i]) {
       return nums[i+2]+nums[i+1]+nums[i]
@@ -12,4 +11,4 @@ var largestPerimeter = function(nums) {
 
 //console.log(largestPerimeter([2,1,2]));
 //console.log(largestPerimeter([1,2,1]));
-console.log(largestPerimeter([3,2,3,4]));
+//console.log(largestPerimeter([3,2,3,4]));
