f444888477cb6dae9305a53d7812d77558397c40
Adam Blomberg <adamablomberg@gmail.com>
2022-10-11 20:06:11 -0500

    largestPerimeterTriangle for array that is > 3

--- interview challenges/largestPerimeterTriangle.js
+++ interview challenges/largestPerimeterTriangle.js
@@ -1,11 +1,15 @@
 //Given an integer array nums, return the largest perimeter of a triangle with a non-zero area,formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
 var largestPerimeter = function(nums) {
-  nums.sort((a,b) => a-b)
-  if(nums[0]+nums[1]<=nums[2]) {
-      return 0;
+  nums.sort((a,b) => b-a)
+  console.log(nums);
+  for(var i = 0; i < nums.length -2; i++){
+    if(nums[i+2]+nums[i+1]>nums[i]) {
+      return nums[i+2]+nums[i+1]+nums[i]
+    }
   }
-  return nums.reduce((partialSum, a) => partialSum + a, 0);
+  return 0;
 };
 
-// console.log(largestPerimeter([2,1,2]));
-// console.log(largestPerimeter([1,2,1]));
+//console.log(largestPerimeter([2,1,2]));
+//console.log(largestPerimeter([1,2,1]));
+console.log(largestPerimeter([3,2,3,4]));
