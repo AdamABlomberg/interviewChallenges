7d2b2cb37871bdec81352369df994924510acdee
Adam Blomberg <adamablomberg@gmail.com>
2023-01-24 23:55:00 -0600

    first attempt on Multiply Strings

---
+++ c sharp interview challenges/MultiplyStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplyStrings;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        Console.WriteLine(Multiply("-5","10"));
+    }
+    static string Multiply(string num1, string num2)
+    {
+        int number1;
+        int number2;
+        int.TryParse(num1, out number1);
+        int.TryParse(num2, out number2);
+        int result = number1*number2;
+        string resultString = result.ToString();
+        return resultString;
+    }
+}
---
+++ c sharp interview challenges/RemoveDuplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RemoveDuplicates;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        int[] array = {1,1,1,3,4,5,6};
+        Console.WriteLine(RemoveDuplicates(array));
+    }
+    static int RemoveDuplicates(int[] nums)
+    {
+        var numberPairs = new Dictionary<int, int>();
+        foreach(int number in nums)
+        {
+            if(!numberPairs.ContainsKey(number))
+            {
+                numberPairs[number] = 1;
+            }
+            else
+            {
+                numberPairs[number]++;
+            }
+        }
+        var keys = numberPairs.Keys;
+        int[] result = new int [nums.Length];
+        int i = 0;
+        foreach (int key in keys)
+        {
+            result[i] = key;
+            i++;
+        }
+        return nums.Length - keys.Count;
+    }
+
+}
